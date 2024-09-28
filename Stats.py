d4ddbabd9bee99f6c5c2c244ff032aea3c7ae6c2
Xzed <stuqadboosty@gmail.com>
2024-09-28 15:32:00 +0100

    Stats.py

--- Stats.py
+++ Stats.py
@@ -13,7 +13,8 @@ def Main():
     info = f"""
     System: {my_system.system} | Node Name: {my_system.node}
     release: {my_system.release} | Version: {my_system.version}
-    Machine: {my_system.machine} | Processor: {my_system.processor}"""
+    Machine: {my_system.machine} | Processor: {my_system.processor}
+    """
 
     print(Colors.green, info, True)
 if __name__ == "__main__":
