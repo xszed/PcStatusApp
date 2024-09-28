744160052897e9da0cb4265f41998706b8d43a2c
Xzed <stuqadboosty@gmail.com>
2024-09-28 15:24:28 +0100

    Add files via upload

---
+++ Stats.py
@@ -0,0 +1,23 @@
+import platform
+import os
+from pystyle import Write, Colors, Box, System
+
+my_system = platform.uname()
+System.Title("PC Status Checker")
+
+def clear_console():
+    os.system('cls')
+
+def Main():
+    clear_console() #Clear terminal
+    info = f"""
+    System: {my_system.system} | Node Name: {my_system.node}
+    release: {my_system.release} | Version: {my_system.version}
+    Machine: {my_system.machine} | Processor: {my_system.processor}"""
+
+    print(Colors.green, info, True)
+
+    press = input("Write anything to close the app >")
+
+if __name__ == "__main__":
+    Main()
